0f175cfeb43ca022e570caa7e0707ea344e49924
suraj <varade.suraj787@gmail.com>
2019-08-01 17:04:55 +0530

    new doc

---
+++ erpnext_prime/prime/doctype/sms_prime/test_sms_prime.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019, Techlift.in and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+import frappe
+import unittest
+
+class TestSMSPrime(unittest.TestCase):
+	pass
